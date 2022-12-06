d64f58a8991a21ce007a85e45670aeadb1b411f0
anand kumar <anandkumar@anands-MacBook-Air.local>
2022-12-05 21:12:42 -0500

    python script for hive connect

---
+++ pyHive/config.py
@@ -0,0 +1,8 @@
+# Credentials
+USERNAME = "ak8288_nyu_edu"
+PORT = 10000
+HIVE_HOST = "localhost"
+
+TABLE = "test1"
+USE_CMD = "use "+USERNAME
+
---
+++ pyHive/query.py
@@ -0,0 +1,42 @@
+from pyhive import hive
+import config
+import sys
+
+class HiveConnection(object):
+    _conn = None
+    
+    def __new__(cls,*args,**kwargs):
+        if not cls._conn:
+            cls._conn = hive.Connection(host=config.HIVE_HOST, port=config.PORT, username=config.USERNAME)
+            return cls._conn
+
+def runQuery(cursor,query):
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def main():
+    args = sys.argv
+    print(args,len(args))
+    if len(args) != 4:
+        print("USAGE: python query.py <CITY> <START_DATE> <END_DATE>")
+        print("Example: python query.py NYC 01/01/2018 01/01/2020")
+        sys.exit()
+    
+    CITY = args[1]
+    START_DATE = args[2];
+    END_DATE = args[3]
+
+    conn = HiveConnection()
+    cursor = conn.cursor()
+    cursor.execute(config.USE_CMD)
+
+    # run query
+    RANGE_QUERY = "SELECT * FROM {} WHERE city='{}' AND crime_date>='{}' AND crime_date<='{}'".format(config.TABLE,CITY,START_DATE,END_DATE)
+    print("range query: ",RANGE_QUERY)
+
+    results = runQuery(cursor,RANGE_QUERY)
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
