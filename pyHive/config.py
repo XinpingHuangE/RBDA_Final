7c515b059e42f01e6ed0b8e96f6b70285a69adf8
anand kumar <anandkumar@anands-MacBook-Air.local>
2022-12-07 18:03:15 -0500

    handle chicago case

--- pyHive/config.py
+++ pyHive/config.py
@@ -3,6 +3,7 @@ USERNAME = "ak8288_nyu_edu"
 PORT = 10000
 HIVE_HOST = "localhost"
 
-TABLE = "test1"
+TABLE = "crimes"
 USE_CMD = "use "+USERNAME
+SET_DATE_FORMAT_CMD = "SET DATEFORMAT mdy"
 
--- pyHive/query.py
+++ pyHive/query.py
@@ -17,16 +17,17 @@ def runQuery(cursor,query):
 
 def main():
     args = sys.argv
-    # print(args,len(args))
-    # if len(args) != 4:
-    #     print("USAGE: python query.py <CITY> <START_DATE> <END_DATE>")
-    #     print("Example: python query.py NYC 01/01/2018 01/01/2020")
-    #     sys.exit()
     
     # INPUT FROM USER
     CITY = input("ENTER CITY (NYC/LA/CHI): ")
     START_DATE = input("ENTER VALID START DATE (ex: 01/01/2012): ")
     END_DATE = input("ENTER VALID END DATE (ex: 01/01/2018): ")
+
+    CITY = CITY.upper()
+
+    # handle chicago case separately
+    if CITY=='CHI':
+        CITY = 'CHICAGO'
 
     start_date_match = re.match("^[0-9]{2}/[0-9]{2}/[0-9]{4}$", START_DATE)
     end_date_match = re.match("^[0-9]{2}/[0-9]{2}/[0-9]{4}$", END_DATE)
@@ -39,10 +40,11 @@ def main():
     conn = HiveConnection()
     cursor = conn.cursor()
     cursor.execute(config.USE_CMD)
-
+    cursor.execute(config.SET_DATE_FORMAT_CMD)
     # run query
     # RANGE_QUERY = "SELECT location, bucket, sum(severity)/count(severity) as crime_rate, avg(latitude) as avg_lat, avg(longitude) as avg_lon FROM {} WHERE city='{}' AND crime_date>='{}' AND crime_date<='{}' GROUP BY location, bucket".format(config.TABLE,CITY,START_DATE,END_DATE)
-    RANGE_QUERY = "SELECT location, sum(severity)/count(severity) as crime_rate, avg(latitude) as avg_lat, avg(longitude) as avg_lon FROM {} WHERE city='{}' AND crime_date>='{}' AND crime_date<='{}' GROUP BY location".format(config.TABLE,CITY,START_DATE,END_DATE)
+    # select location,crime_date from crimes where from_unixtime(unix_timestamp(crime_date , 'MM/dd/yyyy')) >= from_unixtime(unix_timestamp('01/01/2012' ,'MM/dd/yyyy')) and from_unixtime(unix_timestamp(crime_date , 'MM/dd/yyyy')) <= from_unixtime(unix_timestamp('01/01/2014', 'MM/dd/yyyy'));
+    RANGE_QUERY = "SELECT location, sum(severity)/count(severity) as crime_rate, avg(latitude) as avg_lat, avg(longitude) as avg_lon FROM {} WHERE city='{}' AND from_unixtime(unix_timestamp(crime_date , 'MM/dd/yyyy'))>=from_unixtime(unix_timestamp('{}' ,'MM/dd/yyyy')) AND from_unixtime(unix_timestamp(crime_date , 'MM/dd/yyyy'))<=from_unixtime(unix_timestamp('{}', 'MM/dd/yyyy')) GROUP BY location".format(config.TABLE,CITY,START_DATE,END_DATE)
     print("range query: ",RANGE_QUERY)
 
     results = runQuery(cursor,RANGE_QUERY)
